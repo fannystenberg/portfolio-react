e50818c5871d56152cc10229b8203240240b5682
fannystenberg <ffannystenberg@gmail.com>
2023-03-30 12:24:58 +0200

    got started with Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <>
+      <Header />
+      <div className="scroll" aria-hidden="true">
+        <p className="arrow"><i className="fa-solid fa-arrow-down" /></p>
+        <p className="scroll-text">Lets scroll</p>
+      </div>
+      <main>
+        Find me in src/app.js!
+      </main>
+      <Footer />
+    </>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from 'react';
+import '../Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <h2>Time to talk</h2>
+      <div className="about-me">
+        <div className="myProfileImg">
+          <img className="profile-pic" src="../images/img_3060.jpg" alt="profile image of Fanny" />
+        </div>
+        <div className="nameTitle" lang="en-US">
+          <h1 className="name">Fanny Stenberg</h1>
+          <h2 className="title">Frontend Developer</h2>
+        </div>
+      </div>
+      <div className="phoneAndMail" lang="en-US">
+        <p>+46(0) 767 16 47 19</p>
+        <p>ffannystenberg@gmail.com</p>
+      </div>
+      <div className="contact-me">
+        <a hrefLang="en-US" className="icon" title="link to Fanny's Linked In page" href="https://www.linkedin.com/in/fanny-stenberg/" target="_blank" rel="noreferrer">
+          <i aria-hidden="true" className="fab fa-linkedin-in" />
+        </a>
+        <a hrefLang="en-US" className="icon" title="link to Fanny's Github page" href="https://github.com/fannystenberg" target="_blank" rel="noreferrer">
+          <i aria-hidden="true" className="fab fa-github" />
+        </a>
+        <a hrefLang="en-US" className="icon" title="link to Fanny's mail" href="mailto:ffannystenberg@gmail.com" target="_blank" rel="noreferrer">
+          <i aria-hidden="true" className="fa-solid fa-envelope" />
+        </a>
+      </div>
+    </footer>
+  )
+};
+
+export default Footer;
