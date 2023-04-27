14b5a64639002ed0596b7005c35e25ee81c3a68b
fannystenberg <ffannystenberg@gmail.com>
2023-04-27 19:50:19 +0200

    added styled component and array to featured projects

--- src/components/FeaturedProjects.js
+++ src/components/FeaturedProjects.js
@@ -1,46 +1,126 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const FeaturedProjects = (
+const FeaturedProject = (
   { imgSrc, title, description, language1, language2, language3, githubLink, netlifyLink }
 ) => {
   return (
-    <div lang="en-US" className="project">
-      <div className="projectPicture" aria-hidden="true">
-        <img src={imgSrc} alt="" />
-      </div>
-      <div className="project-info" aria-label="Information about the project">
-        <h3 lang="en">{title}</h3>
-        <p lang="en">{description}</p>
-        <ul lang="en" className="project-languages" aria-label="Coding languages used in this project">
+    <ProjectWrapper lang="en-US" className="project">
+      <ImageWrapper aria-hidden="true">
+        <ProjectImage src={imgSrc} alt="" />
+      </ImageWrapper>
+      <ProjectInfo aria-label="Information about the project">
+        <ProjectTitle lang="en">{title}</ProjectTitle>
+        <ProjectDescription lang="en">{description}</ProjectDescription>
+        <ProjectLanguages lang="en" aria-label="Coding languages used in this project">
           <li lang="en" className="showList">HTML</li>
           <li lang="en" className="showList">CSS</li>
           <li lang="en" className="showList">JavaScript</li>
           <li lang="en" className={language1.length <= 2 ? 'hideList' : 'showList'}>{language1}</li>
           <li lang="en" className={language2.length <= 2 ? 'hideList' : 'showList'}>{language2}</li>
           <li lang="en" className={language3.length <= 2 ? 'hideList' : 'showList'}>{language3}</li>
-        </ul>
-        <button
+        </ProjectLanguages>
+        <Button
+          btnColor="#EB5577"
           lang="en"
           type="button"
-          className="view-code"
           title="link to project on Github"
-          aria-label="View code on Github"
           onClick={() => window.open(`https://github.com/fannystenberg/${githubLink}`)}>
           <span><i aria-hidden="true" className="fab fa-github" /></span>
             View the code
-        </button>
-        <button
+        </Button>
+        <Button
+          btnColor="#0B24F5"
           lang="en"
           type="button"
-          className="live-demo"
           title="link to project on Netlify"
-          aria-label="View project live in Netlify"
           onClick={() => window.open(`https://${netlifyLink}.netlify.app/`)}>
           <span><i aria-hidden="true" className="fa-solid fa-globe" /></span>
             Live demo
-        </button>
-      </div>
-    </div>
+        </Button>
+      </ProjectInfo>
+    </ProjectWrapper>
   )
 };
-export default FeaturedProjects;
+export default FeaturedProject;
+
+export const ProjectWrapper = styled.div`
+  margin-top: 24px;
+
+  @media (min-width: 667px) and (max-width: 1024px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  @media (min-width: 1024px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const ImageWrapper = styled.div`
+  margin: 24px 24px 24px 0;
+`;
+
+export const ProjectImage = styled.img`
+  @media (min-width: 667px) and (max-width: 1024px) {
+    width: 184px;
+    height: 280px;
+    object-fit: cover;
+  }
+`;
+
+export const ProjectInfo = styled.div``;
+
+export const ProjectTitle = styled.h3`
+  font-family: 'Satoshi-Variable';
+  font-style: normal;
+  font-weight: 900;
+  font-size: 32px;
+  line-height: 43px;
+  color: #000000;
+`;
+
+export const ProjectDescription = styled.p`
+  font-family: 'EB Garamond';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  line-height: 29px;
+  color: #000000;
+  margin: 24px 24px 24px 0;
+
+  @media (min-width: 667px) and (max-width: 1024px) {
+    max-width: 80%;
+    margin: 24px 0 24px 0;
+  }
+`;
+
+export const ProjectLanguages = styled.ul``;
+
+export const Button = styled.button`
+  display: block;
+  margin: 12px 0px 12px 0;
+  background-color: #F5F5F5;
+  border-radius: 40px;
+  border: none;
+  padding: 12px;
+  font-family: 'Satoshi-Variable';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 27px;
+  color: #333333;
+
+  &:hover {
+    background-color: ${(props) => props.btnColor};
+    color: white;
+  }
+
+  @media (min-width: 667px) and (max-width: 1024px) {
+    display: inline;
+    margin-right: 5px;
+  }
+`;
--- src/components/FeaturedProjectsList.js
+++ src/components/FeaturedProjectsList.js
@@ -1,29 +1,45 @@
 import React from 'react';
-import FeaturedProjects from './FeaturedProjects';
+import styled from 'styled-components';
+import FeaturedProject from './FeaturedProjects';
+
+const Projects = [
+  {
+    imageSrc: '../images/todo.png',
+    title: 'ToDo App',
+    description: 'Designed and built a to-do app with Redux by creating slices, reducers and actions from scratch. I also used Material UI for the first time.',
+    language: ['React', 'JSX', 'Redux'],
+    githubLink: 'project-todos',
+    netlifyLink: 'todo-app-byfanny'
+  },
+  {
+    imageSrc: '../images/quiz.png',
+    title: 'Redux Quiz',
+    description: 'Created a multiple-choice quiz with Redux, the assignment was to interact with the store using selectors and dispatching actions. This was my first project in Redux',
+    language: ['React', 'JSX', 'Redux'],
+    githubLink: 'project-redux-quiz',
+    netlifyLink: 'neon-souffle-d2a8b2'
+  }
+];
 
 const FeaturedProjectsList = () => {
   return (
-    <section lang="en-US" className="featured-projects" title="Fanny's featured projects" aria-label="List of Fanny's featured projects">
-      <h2 lang="en" className="featuredProjectsTitle">Featured Projects</h2>
-      <FeaturedProjects
-        imgSrc="../images/todo.png"
-        title="ToDo App"
-        description="Designed and built a to-do app with Redux by creating slices, reducers and actions from scratch. I also used Material UI for the first time."
-        language1="React"
-        language2="JSX"
-        language3="Redux"
-        githubLink="project-todos"
-        netlifyLink="todo-app-byfanny" />
-      <FeaturedProjects
-        imgSrc="../images/quiz.png"
-        title="Redux Quiz"
-        description="Created a multiple-choice quiz with Redux, the assignment was to interact with the store using selectors and dispatching actions. This was my first project in Redux."
-        language1="React"
-        language2="JSX"
-        language3="Redux"
-        githubLink="project-redux-quiz"
-        netlifyLink="neon-souffle-d2a8b2" />
-      <FeaturedProjects
+    <Section lang="en-US" className="featured-projects" title="Fanny's featured projects" aria-label="List of Fanny's featured projects">
+      <SectionHeader lang="en">Featured Projects</SectionHeader>
+      {Projects.map((project) => {
+        return (
+          <FeaturedProject
+            key={project.imageSrc}
+            imgSrc={project.imageSrc}
+            title={project.title}
+            description={project.description}
+            language1="React"
+            language2="JSX"
+            language3="no"
+            githubLink={project.githubLink}
+            netlifyLink={project.netlifyLink} />
+        )
+      })}
+      <FeaturedProject
         imgSrc="../images/ux-design.png"
         title="Collab with UX-designer"
         description="I was given a design hand-off from UX-design student Linnéa Rådahl. The assignment was to transform that design into code with React."
@@ -32,7 +48,7 @@ const FeaturedProjectsList = () => {
         language3="no"
         githubLink="project-design-handoff"
         netlifyLink="fictious-gym-developedbyfanny" />
-      <FeaturedProjects
+      <FeaturedProject
         imgSrc="../images/movies.png"
         title="Movies"
         description="Created a movie site with multiple pages using React Router and APIs combining useState with useEffect."
@@ -41,7 +57,7 @@ const FeaturedProjectsList = () => {
         language3="Pair-programming"
         githubLink="project-movies"
         netlifyLink="movie-releases-by-fanny-and-fiona" />
-      <FeaturedProjects
+      <FeaturedProject
         imgSrc="../images/happy-thoughts.png"
         title="Happy Thoughts"
         description="Created a Twitter-inspired product, but with less negativity, using React state and fetching/posting data to an API."
@@ -50,7 +66,7 @@ const FeaturedProjectsList = () => {
         language3="no"
         githubLink="project-happy-thoughts"
         netlifyLink="happy-thoughts-byfanny" />
-      <FeaturedProjects
+      <FeaturedProject
         imgSrc="../images/survey.png"
         title="Survey"
         description="Made a survey design and created a Typeform-like product using React state and controlled form."
@@ -59,7 +75,7 @@ const FeaturedProjectsList = () => {
         language3="no"
         githubLink="project-survey"
         netlifyLink="green-survey" />
-      <FeaturedProjects
+      <FeaturedProject
         imgSrc="../images/music-releases.png"
         title="Music releases"
         description="Followed a design and created components using React to build a page which shows new albums and single releases
@@ -69,7 +85,7 @@ const FeaturedProjectsList = () => {
         language3="Pair-programming"
         githubLink="project-music-releases"
         netlifyLink="extraordinary-macaron-fce4c5" />
-      <FeaturedProjects
+      <FeaturedProject
         imgSrc="../images/weather-app.png"
         title="Weather app"
         description="Followed a design and built a simple weather dashboard that tells today's weather, temperature and a 5-day forecast using a weather API."
@@ -78,7 +94,7 @@ const FeaturedProjectsList = () => {
         language3="no"
         githubLink="project-weather-app"
         netlifyLink="weather-app-byannafannyfiona" />
-      <FeaturedProjects
+      <FeaturedProject
         imgSrc="../images/guess-who.png"
         title="Guess who the game"
         description="Created a digital version of the classic board game Guess who."
@@ -87,7 +103,7 @@ const FeaturedProjectsList = () => {
         language3="no"
         githubLink="project-guess-who"
         netlifyLink="guess-who-byfannystenberg" />
-      <FeaturedProjects
+      <FeaturedProject
         imgSrc="../images/chatbot.png"
         title="Chatbot built in JS"
         description="Built a chatbot that gives you recommendations for restaurants in Stockholm depending on your pick of cuisine. This was my first project in JavaScript."
@@ -96,8 +112,29 @@ const FeaturedProjectsList = () => {
         language3="no"
         githubLink="project-chatbot"
         netlifyLink="theguide-chatbot" />
-    </section>
+    </Section>
   )
 };
 
 export default FeaturedProjectsList;
+
+export const Section = styled.section`
+  margin-left: 24px;
+
+  @media (min-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+  }
+`;
+
+export const SectionHeader = styled.h2`
+  font-family: 'Satoshi-Variable';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 72px;
+  line-height: 80px;
+  color: #0B24F5;
+  margin: 24px 24px 24px 0;
+`;
